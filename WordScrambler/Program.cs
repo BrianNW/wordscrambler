b7c3936f4635d9b6092c923e7fe4a1171965924d
Brian <briantechdom@gmail.com>
2018-05-09 10:03:34 -0700

    Added logic

--- WordScrambler/Program.cs
+++ WordScrambler/Program.cs
@@ -10,6 +10,45 @@ namespace WordScrambler
     {
         static void Main(string[] args)
         {
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.WriteLine("Please enter the option - F for File and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrambled words file name: ");
+                        break;
+                    case "M":
+                        Console.Write("Enter Scrambled words manually: ");
+                        break;
+                    default:
+                        Console.Write("Option was not recognized");
+                        break;
+                }
+                var continueWordUnscrambledDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueWordUnscrambledDecision = (Console.ReadLine() ?? string.Empty);
+                } while {
+                    !continueWordUnscrambledDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+                    !continueWordUnscrambledDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+                } while ();
+            }
+            private static void ExecuteScrambeWordsInFileScenario()
+            {
+                throw new NotImplementedException();
+            }
+
+            private static void ExecuteScrambledWordsInFileScenario()
+            {
+                throw new NotImplementedException();
+            }
+
+
         }
     }
 }
