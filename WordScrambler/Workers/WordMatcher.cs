806874e11c3aea671d04cf521d15179dec3a2cf7
Brian <briantechdom@gmail.com>
2018-06-01 15:12:33 -0700

    Added wordmatcher logic

--- WordScrambler/Workers/WordMatcher.cs
+++ WordScrambler/Workers/WordMatcher.cs
@@ -9,9 +9,51 @@ namespace WordScrambler.Workers
 {
     class WordMatcher
     {
-        internal static List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        public static List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
         {
-            throw new NotImplementedException();
+            var matchedWords = new List<MatchedWord>();
+
+            foreach(var scrambledWord in scrambledWords)
+            {
+
+                foreach (var word in wordList)
+                {
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scrambledWord, word));         
+                    } else
+                    {
+                        var scrambedWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambedWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord = new string(scrambedWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if(sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                        }
+                    }
+                }
+
+            }
+
+            return matchedWords;
+        }
+
+        private MatchedWord BuildMatchedWord(string scrambledWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWord = scrambledWord,
+                Word = word
+            };
+
+            return matchedWord;
+            
         }
     }
 }
